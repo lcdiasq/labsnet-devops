c12a276ba3310cec0807360e7993dd16346ba9eb
lcdiasq <lcdiasq@gmail.com>
2026-04-04 00:55:00 -0300

    feat: add monitoring and QA pipeline

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from app.routes import health
+import logging
 
+# Configuração básica de logs
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-app = FastAPI()
+app = FastAPI(title="LabsNet API", version="1.0")
 
+@app.on_event("startup")
+def startup_event():
+    logger.info("🚀 Aplicação iniciada com sucesso")
 
+@app.on_event("shutdown")
+def shutdown_event():
+    logger.info("🛑 Aplicação finalizada")
+
+# Rotas
 app.include_router(health.router)
-
-
-@app.get("/")
-def root():
-    return {"message": "LabsNet DevOps API"}
