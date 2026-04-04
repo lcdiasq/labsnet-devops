2fd965c4501e2cf95a539b3fff93a5e226b00902
lcdiasq <lcdiasq@gmail.com>
2026-04-04 01:18:22 -0300

    feat: add devops dashboard frontend

--- app/main.py
+++ app/main.py
@@ -1,20 +1,29 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.routes import health
 import logging
 
-# Configuração básica de logs
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI(title="LabsNet API", version="1.0")
+app = FastAPI(title="LabsNet DevOps", version="1.0")
 
 @app.on_event("startup")
 def startup_event():
-    logger.info("🚀 Aplicação iniciada com sucesso")
+    logger.info("Aplicação iniciada")
 
 @app.on_event("shutdown")
 def shutdown_event():
-    logger.info("🛑 Aplicação finalizada")
+    logger.info("Aplicação finalizada")
 
-# Rotas
+# Rotas API
 app.include_router(health.router)
+
+# Frontend (servir arquivos estáticos)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/")
+def dashboard():
+    return FileResponse("app/static/index.html")
+
